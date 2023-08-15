beed37736c52ae3c1d98645a7d73f083a60a6229
RuchirTrivedi <ruckstrivedi87@gmail.com>
2023-08-15 11:49:42 -0400

    added logger to login page test class

--- src/test/java/com/acti/testcases/LoginPageTest.java
+++ src/test/java/com/acti/testcases/LoginPageTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.acti.pages.EnterTimePage;
+import com.aventstack.extentreports.ExtentTest;
 
 public class LoginPageTest extends BaseTest
 {	
@@ -11,9 +12,12 @@ public class LoginPageTest extends BaseTest
 	@Test(priority = 1)
 	public void testLoginPageTitle()
 	{
-		
+		logger = report.createTest("Test Login Page Title");
+		String actual = "actiTIME - Login";
 		String expected = lp.getLoginPageTitle();
 		System.out.println(expected);
+		Assert.assertEquals(actual, expected);
+		logger.pass("Actual title matched the expected");
 		
 	}
 	
@@ -21,28 +25,39 @@ public class LoginPageTest extends BaseTest
 	@Test(priority = 2)
 	public void testActiLogoDisplayed()
 	{
+		logger = report.createTest("Test actiTime logo");
 		boolean expected = lp.acTiImgisDisplyed();
 		System.out.println(expected);
+		logger.pass("Acti Logo is dispalyed");
+		
 	}
 	
 	@Test(priority = 3)
 	public void testActiLinkDisplayed()
 	{
+		logger = report.createTest("Test actiTime Link");
 		boolean flag = lp.acTiImgisDisplyed();
 		System.out.println(flag);
-		
+		logger.pass("ActiTime Link is Displayed");
 	}
 	
 	@Test(priority = 4)
 	public void testLoginFunction()
 	{
+		logger = report.createTest("Test Login Function");
 		lp.enterUsername("admin");
+		logger.pass("Entered the username");
 		lp.enterPassword("manager");
+		logger.pass("Entered the password");
 		lp.clickLogin();
+		logger.pass("Cliked on LoginButton");
 		EnterTimePage ep = new EnterTimePage();
 		String user = ep.getUserLoggedIn();
 		Assert.assertTrue(user.contains("John"));
+		System.out.println(user);
+		logger.pass("User Logged in name contain John");
 		ep.ClickLogout();
+		logger.pass("Looged out from Applications");
 		
 	}
 	
