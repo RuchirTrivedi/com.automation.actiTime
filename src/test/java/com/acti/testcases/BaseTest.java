3a2cb19b43330f3151148ac01fb59d296a3eefe2
RuchirTrivedi <ruckstrivedi87@gmail.com>
2023-08-15 11:30:26 -0400

    added extent report code to the base test

--- src/test/java/com/acti/testcases/BaseTest.java
+++ src/test/java/com/acti/testcases/BaseTest.java
@@ -2,14 +2,34 @@ package com.acti.testcases;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import com.acti.driver.DriverManager;
 import com.acti.pages.LoginPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class BaseTest extends DriverManager 
 {
-LoginPage lp;
 	
+	protected static ExtentSparkReporter extent;
+	protected static ExtentReports report;
+	protected static ExtentTest logger;
+	LoginPage lp;
+	
+
+
+	@BeforeSuite
+	public void setUpReport()
+	{
+		extent = new ExtentSparkReporter("./Reports/actiindex.html");
+		report = new ExtentReports();
+		report.attachReporter(extent);
+		
+	}
+
+
 	@BeforeMethod
 	public void PreTest()
 	{
@@ -20,6 +40,7 @@ LoginPage lp;
 	@AfterMethod
 	public void tearDown()
 	{
+		report.flush();
 		quitBrowser();
 	}
 }
